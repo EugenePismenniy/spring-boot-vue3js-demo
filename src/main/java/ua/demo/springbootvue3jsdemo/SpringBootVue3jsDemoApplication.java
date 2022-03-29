2c7363579217a716c43fef8664be736756564b29
evhen <evgeniy.pismenny@gmail.com>
2022-03-29 11:40:42 +0300

    init

---
+++ src/main/java/ua/demo/springbootvue3jsdemo/SpringBootVue3jsDemoApplication.java
@@ -0,0 +1,13 @@
+package ua.demo.springbootvue3jsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVue3jsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVue3jsDemoApplication.class, args);
+    }
+
+}
