dc050f55892fe312ce57ea0ebda1f55e8bf0e6ef
evhen <evgeniy.pismenny@gmail.com>
2022-03-29 15:45:53 +0300

    simple vue3 app

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h3>Data List</h3>
+    <data-list :data-list="dataList"/>
+  </div>
+</template>
+
+<script>
+
+import DataList from "@/components/DataList";
+
+export default {
+  name: 'App',
+  components: {
+    DataList
+  },
+  data() {
+    return {
+      dataList: [
+        {id: 1, name: 'Some name', value: 1234},
+        {id: 2, name: 'Some name 2', value: 2234},
+        {id: 3, name: 'Some name 3', value: 3234},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  padding: 15px 0;
+}
+</style>
---
+++ src/frontend/src/components/DataList.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="dl in dataList" :key="dl.id">
+        <td>{{ dl.id }}</td>
+        <td>{{ dl.name }}</td>
+        <td>{{ dl.value }}</td>
+      </tr>
+    </tbody>
+
+  </table>
+
+
+</template>
+
+<script>
+export default {
+  name: "data-list",
+  props: {
+    dataList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
